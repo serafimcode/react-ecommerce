8623cf6cf0e93e738f7ff1be72e7a30764920345
Serafim <serafim.code@gmail.com>
2023-02-24 12:37:34 +0700

    created authentication page component and sign in form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,46 +1,80 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBR5KX4ZCj3muwbBf_jZSQl2qH6DAQl3-Y",
-  authDomain: "react-ecommerce-cef9e.firebaseapp.com",
-  projectId: "react-ecommerce-cef9e",
-  storageBucket: "react-ecommerce-cef9e.appspot.com",
-  messagingSenderId: "421581298252",
-  appId: "1:421581298252:web:777529cb04f38b0499b2e9"
+  apiKey: 'AIzaSyBR5KX4ZCj3muwbBf_jZSQl2qH6DAQl3-Y',
+  authDomain: 'react-ecommerce-cef9e.firebaseapp.com',
+  projectId: 'react-ecommerce-cef9e',
+  storageBucket: 'react-ecommerce-cef9e.appspot.com',
+  messagingSenderId: '421581298252',
+  appId: '1:421581298252:web:777529cb04f38b0499b2e9'
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
-  prompt: "select_account"
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-  const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, "users", userAuth.uid);
+
   const userSnapshot = await getDoc(userDocRef);
 
-  // if user data does not exist
-  if(!userSnapshot.exists()) {
-    // create/set the document with the data from userAuth in my collection
-    const { displayName, email} = userAuth;
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
     const createdAt = new Date();
+
     try {
       await setDoc(userDocRef, {
-        displayName, email, createdAt
-      })
-    } catch (e) {
-      console.log('error creating the user', e.message);
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
     }
   }
-  // if user exists
+
   return userDocRef;
-}
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
